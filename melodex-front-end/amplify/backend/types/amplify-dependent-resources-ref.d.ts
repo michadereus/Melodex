732cad7b6d796598f57a839f7ff78fbf2b82d740
michadereus <michael.dereus@outlook.com>
2025-03-22 01:53:19 -0500

    removed amplify from gitignore and initial setup for amplify hosting with cotinuous integration

---
+++ melodex-front-end/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "songranker06d382dc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "SongRankerUserData": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
